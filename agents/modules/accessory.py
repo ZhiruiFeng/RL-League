ca3326e762718bd4a1fa8d11ce47087e983e3a2a
ZhiruiFeng <fihezro@gmail.com>
2018-03-29 23:17:26 -0700

    adjust accessory

--- agents/modules/accessory.py
+++ agents/modules/accessory.py
@@ -139,3 +139,52 @@ class LogQValues(Log):
         self.mean = self.data[1]
         self.max = self.data[2]
         self.std = self.data[3]
+
+class LinearControlSignal:
+    """
+    A control signal that changes linearly over time.
+    This is used to change e.g. the learning-rate for the optimizer
+    of the Neural Network, as well as other parameters.
+
+    TensorFlow has functionality for doing this, but it uses the
+    global_step counter inside the TensorFlow graph, while we
+    want the control signals to use a state-counter for the
+    game-environment. So it is easier to make this in Python.
+    """
+
+    def __init__(self, start_value, end_value, num_iterations, repeat=False):
+        """
+        Create a new object.
+        :param start_value:
+            Start-value for the control signal.
+        :param end_value:
+            End-value for the control signal.
+        :param num_iterations:
+            Number of iterations it takes to reach the end_value
+            from the start_value.
+        :param repeat:
+            Boolean whether to reset the control signal back to the start_value
+            after the end_value has been reached.
+        """
+
+        # Store arguments in this object.
+        self.start_value = start_value
+        self.end_value = end_value
+        self.num_iterations = num_iterations
+        self.repeat = repeat
+
+        # Calculate the linear coefficient.
+        self._coefficient = (end_value - start_value) / num_iterations
+
+    def get_value(self, iteration):
+        """Get the value of the control signal for the given iteration."""
+
+        if self.repeat:
+            iteration %= self.num_iterations
+
+        if iteration < self.num_iterations:
+            value = iteration * self._coefficient + self.start_value
+        else:
+            value = self.end_value
+
+        return value
